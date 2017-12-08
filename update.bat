1c15987a98cd2b01e133e46bbd9b41ad33dc2b2b
TFA <theforgottenarchitect@gmail.com>
2017-12-08 14:39:48 -0600

    fixes bat file

--- update.bat
+++ update.bat
@@ -1,5 +1,5 @@
-D:\Games\Steam\steamapps\common\GarrysMod\bin\gmad.exe create -folder %~dp0 -out %~dp0/gma.gma -warninvalid
-D:\Games\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe update -id 415143062 -addon %~dp0/gma.gma
-D:\Games\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe update -id 415143062 -icon %~dp0/icon.jpg
-del gma.gma
+..\..\..\bin\gmad.exe create -out C:\gma.gma -warninvalid -folder "%~dp0
+..\..\..\bin\gmpublish.exe update -id 415143062 -addon "C:\gma.gma"
+..\..\..\bin\gmpublish.exe update -id 415143062 -icon "%~dp0/icon.jpg"
+del C:\gma.gma
 pause
