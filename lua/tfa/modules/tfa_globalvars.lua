6182f0f35df8998f2ece2c3853fc10e89251a645
TFA <theforgottenarchitect@gmail.com>
2018-05-02 15:51:58 -0500

    deleted tfa base

--- lua/tfa/modules/tfa_globalvars.lua
+++
@@ -1,2 +0,0 @@
-uAng = Angle(0,0,0)
-bvec = Vector()
--- lua/tfa/modules/tfa_turretphysics.lua
+++
@@ -1,5 +0,0 @@
-hook.Add("EntityTakeDamage","TFA_TurretPhysics", function(ent,dmg)
-	if ent:GetClass() == "npc_turret_floor" then
-		ent:TakePhysicsDamage( dmg )
-	end
-end)
