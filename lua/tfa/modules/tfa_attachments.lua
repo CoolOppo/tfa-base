a3f34257b53e28121e5cf37dc9ba5637f8477d1d
TheForgottenArchitect <theforgottenarchitect@gmail.com>
2018-06-19 00:10:27 -0500

    attachment inheritance

--- lua/tfa/modules/tfa_attachments.lua
+++ lua/tfa/modules/tfa_attachments.lua
@@ -104,6 +104,14 @@ end
 TFA.Attachments.Path = "tfa/att/"
 TFA_ATTACHMENT_ISUPDATING = false
 
+local function basefunc(t, k)
+	if k == "Base" then return end
+	if t.Base then
+		local bt = TFA.Attachments.Atts[t.Base]
+		if bt then return bt[k] end
+	end
+end
+
 function TFAUpdateAttachments()
 	if SERVER then
 		net.Start("TFA_Attachment_Reload")
@@ -135,9 +143,9 @@ function TFAUpdateAttachments()
 			include(v)
 		end
 
-		if ATTACHMENT.Model and type(ATTACHMENT.Model) == "string" and ATTACHMENT.Model ~= "" then
-			util.PrecacheModel(ATTACHMENT.Model)
-		end
+		setmetatable(ATTACHMENT, {
+			__index = basefunc
+		})
 
 		TFARegisterAttachment(ATTACHMENT)
 		ATTACHMENT = nil
