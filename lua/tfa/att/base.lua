aa9f306479778ae884720383f4af7c2b830c4427
TheForgottenArchitect <theforgottenarchitect@gmail.com>
2018-06-23 19:17:56 -0500

    base att updated

--- lua/tfa/att/base.lua
+++ lua/tfa/att/base.lua
@@ -4,33 +4,15 @@ end
 
 ATTACHMENT.Name = "Base Attachment"
 ATTACHMENT.ShortName = nil --Abbreviation, 5 chars or less please
---ATTACHMENT.ID = "base" -- normally this is just your filename
-ATTACHMENT.Description = { TFA.Attachments.Colors["+"], "Does something good", TFA.Attachments.Colors["-"], "Does something bad" }
+ATTACHMENT.Description = {} --TFA.Attachments.Colors["+"], "Does something good", TFA.Attachments.Colors["-"], "Does something bad" }
 ATTACHMENT.Icon = nil --Revers to label, please give it an icon though!  This should be the path to a png, like "entities/tfa_ammo_match.png"
-
-ATTACHMENT.WeaponTable = {
-	["Primary"] = {}
-}
+ATTACHMENT.WeaponTable = {} --put replacements for your SWEP talbe in here e.g. ["Primary"] = {}
 
 function ATTACHMENT:Attach(wep)
 end
 
 function ATTACHMENT:Detach(wep)
 end
-
---Toss basically anything into here, inline with this format:
---[[
-ATTACHMENT.WeaponTable = {
-	["Primary"] = {
-		["Damage"] = function(wep,val) return val * 1.15 end
-	},
-	["VElements"] = {
-		["sample"] = {
-			["active"] = true
-		}
-	}
-}
-]]--
 
 if not TFA_ATTACHMENT_ISUPDATING then
 	TFAUpdateAttachments()
