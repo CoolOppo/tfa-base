f9150eb32ad790e757213f00665372c3b9bc5e38
YuRaNnNzZZ <medved609@yandex.ru>
2018-11-16 21:45:29 +0300

    fix for viewbob working when game is paused in singleplayer

--- lua/tfa/modules/tfa_vm.lua
+++ lua/tfa/modules/tfa_vm.lua
@@ -4,13 +4,22 @@ cheats = GetConVar("sv_cheats")
 vec = Vector()
 ang = Angle()
 
+local sp = game.SinglePlayer()
+local IsGameUIVisible = gui and gui.IsGameUIVisible
+
 hook.Add("PreDrawViewModel", "TFACalculateViewmodel", function(vm, ply, wep)
 	if not IsValid(wep) or not wep:IsTFA() then return end
+
 	local st = SysTime()
 	st_old = st_old or st
+
 	local delta = st - st_old
 	st_old = st
+
+	if sp and IsGameUIVisible and IsGameUIVisible() then return end
+
 	delta = delta * game.GetTimeScale() * (cheats:GetBool() and host_ts:GetFloat() or 1)
+
 	wep:Sway(vec, ang, delta)
 	wep:CalculateViewModelOffset(delta)
 	wep:CalculateViewModelFlip()
