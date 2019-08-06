c3592130b4d2723f0281bfc4b666e80ac6d4456f
YuRaNnNzZZ <medved609@yandex.ru>
2019-08-07 00:33:16 +0300

    allow custom bounce sound for timed explosives

--- lua/entities/tfa_exp_timed.lua
+++ lua/entities/tfa_exp_timed.lua
@@ -24,9 +24,14 @@ AddCSLuaFile()
 ENT.Base = "tfa_exp_base"
 ENT.PrintName = "Timed Explosive"
 
+ENT.BounceSound = Sound("HEGrenade.Bounce")
+
 function ENT:PhysicsCollide(data, phys)
 	if data.Speed > 60 then
-		self:EmitSound(Sound("HEGrenade.Bounce"))
+		if self.BounceSound then
+			self:EmitSoundNet(self.BounceSound)
+		end
+
 		local impulse = (data.OurOldVelocity - 2 * data.OurOldVelocity:Dot(data.HitNormal) * data.HitNormal) * 0.25
 		phys:ApplyForceCenter(impulse)
 	end
