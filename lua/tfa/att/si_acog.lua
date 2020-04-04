f3f9bf7dc7a68094de35b663f5e257f15e1399db
YuRaNnNzZZ <medved609@yandex.ru>
2020-04-04 03:15:41 +0300

    rt scopes rework, base

--- lua/tfa/att/si_acog.lua
+++ lua/tfa/att/si_acog.lua
@@ -23,6 +23,7 @@ if not ATTACHMENT then
 	ATTACHMENT = {}
 end
 
+ATTACHMENT.Base = "si_rt_base"
 ATTACHMENT.Name = "ACOG"
 --ATTACHMENT.ID = "base" -- normally this is just your filename
 ATTACHMENT.Description = { TFA.Attachments.Colors["="], "4x zoom", TFA.Attachments.Colors["-"], "20% higher zoom time",  TFA.Attachments.Colors["-"], "10% slower aimed walking" }
@@ -54,71 +55,13 @@ ATTACHMENT.WeaponTable = {
 	["IronSightTime"] = function( wep, val ) return val * 1.20 end,
 	["IronSightMoveSpeed"] = function(stat) return stat * 0.9 end,
 	["RTOpaque"] = true,
-	["RTMaterialOverride"] = -1
+	["RTMaterialOverride"] = -1,
+
+	["RTScopeFOV"] = 90 / 4 / 2, -- Default FOV / Scope Zoom / screenscale
+
+	["RTReticleMaterial"] = Material("scope/gdcw_acog"),
+	["RTReticleScale"] = 1,
 }
-
-local shadowborder = 256
-
-local cd = {}
-
-local myret
-local myshad
-
-function ATTACHMENT:Attach(wep)
-	if not IsValid(wep) then return end
-	wep.RTCodeOld = wep.RTCodeOld or wep.RTCode
-	wep.RTCode = function( myself , rt, scrw, scrh)
-		if not IsValid(myself:GetOwner()) then return end
-		if not myret then
-			myret = Material("scope/gdcw_scopesightonly")
-		end
-		if not myshad then
-			myshad = Material( "vgui/scope_shadowmask_test")
-		end
-
-		render.OverrideAlphaWriteEnable(true, true)
-		surface.SetDrawColor(color_white)
-		surface.DrawRect(-512, -512, 1024, 1024)
-		render.OverrideAlphaWriteEnable(true, true)
-		local ang = myself:GetOwner():EyeAngles()
-		cd.angles = ang
-		cd.origin = myself:GetOwner():GetShootPos()
-		local rtw, rth = 512, 512
-		cd.x = 0
-		cd.y = 0
-		cd.w = rtw
-		cd.h = rth
-		cd.fov = fov
-		cd.drawviewmodel = false
-		cd.drawhud = false
-		render.Clear(0, 0, 0, 255, true, true)
-		render.SetScissorRect(0, 0, rtw, rth, true)
-
-		if myself.CLIronSightsProgress > 0.005 then
-			render.RenderView(cd)
-		end
-
-		render.SetScissorRect(0, 0, rtw, rth, false)
-		render.OverrideAlphaWriteEnable(false, true)
-		cam.Start2D()
-		draw.NoTexture()
-		surface.SetDrawColor(ColorAlpha(color_black, 255 * (1 - myself.CLIronSightsProgress)))
-		surface.DrawRect(0, 0, rtw, rth)
-		surface.SetMaterial(myret)
-		surface.SetDrawColor(color_white)
-		surface.DrawTexturedRect(128, 128, 256, 256)
-		surface.SetMaterial(myshad)
-		surface.SetDrawColor(color_white)
-		surface.DrawTexturedRect(-shadowborder, -shadowborder, shadowborder * 2 + 512 , shadowborder * 2 + 512 )
-		cam.End2D()
-	end
-end
-
-function ATTACHMENT:Detach(wep)
-	if not IsValid(wep) then return end
-	wep.RTCode = wep.RTCodeOld
-	wep.RTCodeOld = nil
-end
 
 if not TFA_ATTACHMENT_ISUPDATING then
 	TFAUpdateAttachments()
