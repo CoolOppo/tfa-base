e8d90991f19d111e69e8b5f011edf46861f8ef83
YuRaNnNzZZ <medved609@yandex.ru>
2020-05-07 19:13:42 +0300

    Only print message in chat when using unpacked version, do not trigger for downloaded Git repo with all files intact.

--- lua/tfa/modules/tfa_envcheck.lua
+++ lua/tfa/modules/tfa_envcheck.lua
@@ -3,11 +3,17 @@ local EmptyFunc = function() end
 local debugInfoTbl = debug.getinfo(EmptyFunc)
 
 local function checkEnv(plyIn)
+	local printFunc = chat and chat.AddText or print
+
 	if game.SinglePlayer() then
-		if chat and chat.AddText
-				and debugInfoTbl.short_src:StartWith("addons") -- unpacked version
-				and not file.Exists(debugInfoTbl.short_src:GetPathFromFilename():Replace("lua/tfa/modules/", "") .. ".git", "GAME") then -- non-git
-			chat.AddText("[TFA Base] You are using unpacked version of TFA Base.\nWe only provide support for Workshop and Git versions.")
+		local shortsrc = debugInfoTbl.short_src
+
+		if shortsrc:StartWith("addons") then -- legacy/unpacked addon
+			local addonRootFolder = shortsrc:GetPathFromFilename():Replace("lua/tfa/modules/", "")
+
+			if not (file.Exists(addonRootFolder .. ".git", "GAME") or file.Exists(addonRootFolder .. "LICENSE", "GAME")) then -- assume unpacked version by missing both .git and LICENSE files, which are ignored by gmad.exe
+				printFunc("[TFA Base] You are using unpacked version of TFA Base.\nWe only provide support for Workshop and Git versions.")
+			end
 		end
 	else
 		local activeGamemode = engine.ActiveGamemode()
@@ -19,8 +25,6 @@ local function checkEnv(plyIn)
 			print("[TFA Base] You are running the base on DarkRP or DarkRP-derived gamemode. We can't guarantee that it will work correctly with any possible addons the server might have installed (especially the paid ones), so we don't provide support for RP gamemodes/servers. If you've encountered a conflict error with another addon, it's most likely that addon's fault. DO NOT CONTACT US ABOUT THAT!")
 
 			if TFA_BASE_VERSION <= 4.034 then -- seems to be common problem with SWRP servers
-				local printFunc = chat and chat.AddText or print
-
 				printFunc("[TFA Base] You have installed both SV/SV2 and Reduxed versions of the base. Make sure you are using only one version at the same time.")
 			end
 		end
