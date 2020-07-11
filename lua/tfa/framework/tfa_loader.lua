11b84fc18fcbcfd464887f0103eaffef3bbab6d0
YuRaNnNzZZ <medved609@yandex.ru>
2020-07-11 13:54:01 +0300

    changelog/version bump

--- lua/tfa/framework/tfa_loader.lua
+++ lua/tfa/framework/tfa_loader.lua
@@ -24,16 +24,11 @@ if SERVER then AddCSLuaFile() end
 TFA = TFA or {}
 
 local do_load = true
-local version = 4.551
-local version_string = "4.5.5.1"
+local version = 4.56
+local version_string = "4.5.6"
 local changelog = [[
-	+ Added TFA_BulletPenetration hook, called when bullet hits/penetrates a surface
-	* Reverted ironsights FOV compensation changes - new compensation only applies to viewmodels now
-	* Exposed more variables to stat cache (various sounds, sck elements bodygroups)
-	* Fixed performance degradation when player holds a non-TFA weapon in singleplayer
-	+ Added muzzleflash smoke toggle convar (Q -> Options -> Performance -> Use Muzzle Flash Smoke)
-	* Fixed non-ballistics bullet tracers for akimbo weapons
-	+ Added bash damage display to inspection screen (when available)
+	+ Added ammo entities for SMG grenades (1 and 5 grenades for small and large pickups)
+	+ Server settings menu now work in multiplayer (rewritten to use networked controls)
 ]]
 
 local function testFunc()
