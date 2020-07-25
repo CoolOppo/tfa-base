a514c4028c50611a4ae6213083869786573870ac
YuRaNnNzZZ <medved609@yandex.ru>
2020-07-26 02:45:14 +0300

    Merge branch 'master' of https://gitlab.com/tfa-devs/tfa-base

--- lua/tfa/framework/tfa_loader.lua
+++ lua/tfa/framework/tfa_loader.lua
@@ -24,13 +24,11 @@ if SERVER then AddCSLuaFile() end
 TFA = TFA or {}
 
 local do_load = true
-local version = 4.571
-local version_string = "4.5.7.1"
+local version = 4.58
+local version_string = "4.5.8"
 local changelog = [[
-	+ Added localization support. English and Russian locales are bundled by default.
-	* Fixed weapons not spawning in TTT (by Anairkoen Schno)
-	* Blocked new ammo pickups (SMG grenades) from being pocketed in DarkRP
-	* Fixed networked menu not working in Local/P2P servers
+	+ Added material proxy for tinting envmaps with ambient lighting (TFA_CubemapTint)
+	+ Added option to debug currently playing anination (Admin-only)
 ]]
 
 local function testFunc()
