2c1d429f5a61667af41a9c0bbb2092e233107451
YuRaNnNzZZ <medved609@yandex.ru>
2020-08-14 04:39:31 +0300

    moved status think from think2 to own function

--- lua/weapons/tfa_gun_base/sh_events.lua
+++ lua/weapons/tfa_gun_base/sh_events.lua
@@ -116,3 +116,186 @@ function SWEP:EmitSoundSafe(snd)
 		if IsValid(self) and snd then self:EmitSound(snd) end
 	end)
 end
+
+local ct, is, stat, statend, finalstat, waittime, lact
+
+function SWEP:ProcessStatus()
+	local self2 = self:GetTable()
+
+	ct = l_CT()
+
+	is = self:GetIronSights()
+	stat = self:GetStatus()
+	statend = self:GetStatusEnd()
+
+	if stat == TFA.Enum.STATUS_FIDGET and is then
+		self:SetStatusEnd(0)
+		self2.Idle_Mode_Old = self2.Idle_Mode
+		self2.Idle_Mode = TFA.Enum.IDLE_BOTH
+		self2.ChooseIdleAnim(self)
+
+		if sp then
+			self:CallOnClient("ChooseIdleAnim", "")
+		end
+
+		self2.Idle_Mode = self2.Idle_Mode_Old
+		self2.Idle_Mode_Old = nil
+		statend = -1
+	end
+
+	if stat ~= TFA.Enum.STATUS_IDLE and ct > statend then
+		finalstat = TFA.Enum.STATUS_IDLE
+
+		--Holstering
+		if stat == TFA.Enum.STATUS_HOLSTER then
+			finalstat = TFA.Enum.STATUS_HOLSTER_READY
+			self:SetStatusEnd(ct + 0.0)
+		elseif stat == TFA.Enum.STATUS_HOLSTER_READY then
+			self2.FinishHolster(self)
+			finalstat = TFA.Enum.STATUS_HOLSTER_FINAL
+			self:SetStatusEnd(ct + 0.6)
+		elseif stat == TFA.Enum.STATUS_RELOADING_SHOTGUN_START_SHELL then
+			--Shotgun Reloading from empty
+			if not self2.IsJammed(self) then
+				self2.TakePrimaryAmmo(self, 1, true)
+				self2.TakePrimaryAmmo(self, -1)
+			end
+
+			if self2.Ammo1(self) <= 0 or self:Clip1() >= self2.GetPrimaryClipSize(self) or self:GetShotgunCancel() then
+				finalstat = TFA.Enum.STATUS_RELOADING_SHOTGUN_END
+				local _, tanim = self2.ChooseShotgunPumpAnim(self)
+				self:SetStatusEnd(ct + self:GetActivityLength(tanim))
+				self:SetShotgunCancel(false)
+
+				if not self:GetShotgunCancel() then
+					self:SetJammed(false)
+				end
+			else
+				lact = self:GetLastActivity()
+				waittime = self2.GetActivityLength(self, lact, false) - self2.GetActivityLength(self, lact, true)
+
+				if waittime > 0.01 then
+					finalstat = TFA.Enum.STATUS_RELOADING_WAIT
+					self:SetStatusEnd(ct + waittime)
+				else
+					finalstat = self2.LoadShell(self)
+				end
+
+				self:SetJammed(false)
+				--finalstat = self:LoadShell()
+				--self:SetStatusEnd( self:GetNextPrimaryFire() )
+			end
+		elseif stat == TFA.Enum.STATUS_RELOADING_SHOTGUN_START then
+			--Shotgun Reloading
+			finalstat = self2.LoadShell(self)
+		elseif stat == TFA.Enum.STATUS_RELOADING_SHOTGUN_LOOP then
+			self2.TakePrimaryAmmo(self, 1, true)
+			self2.TakePrimaryAmmo(self, -1)
+			lact = self:GetLastActivity()
+
+			if self2.GetActivityLength(self, lact, true) < self2.GetActivityLength(self, lact, false) - 0.01 then
+				sht = self2.GetStat(self, "ShellTime")
+
+				if sht then
+					sht = sht / self2.GetAnimationRate(self, ACT_VM_RELOAD)
+				end
+
+				waittime = (sht or self2.GetActivityLength(self, lact, false)) - self2.GetActivityLength(self, lact, true)
+			else
+				waittime = 0
+			end
+
+			if waittime > 0.01 then
+				finalstat = TFA.Enum.STATUS_RELOADING_WAIT
+				self:SetStatusEnd(ct + waittime)
+			else
+				if self2.Ammo1(self) <= 0 or self:Clip1() >= self:GetPrimaryClipSize() or self:GetShotgunCancel() then
+					finalstat = TFA.Enum.STATUS_RELOADING_SHOTGUN_END
+					local _, tanim = self2.ChooseShotgunPumpAnim(self)
+					self:SetStatusEnd(ct + self:GetActivityLength(tanim))
+					self:SetShotgunCancel(false)
+				else
+					finalstat = self2.LoadShell(self)
+				end
+			end
+		elseif stat == TFA.Enum.STATUS_RELOADING then
+			self2.CompleteReload(self)
+			lact = self:GetLastActivity()
+			waittime = self2.GetActivityLength(self, lact, false) - self2.GetActivityLength(self, lact, true)
+
+			if waittime > 0.01 then
+				finalstat = TFA.Enum.STATUS_RELOADING_WAIT
+				self:SetStatusEnd(ct + waittime)
+			end
+		elseif stat == TFA.Enum.STATUS_SILENCER_TOGGLE then
+			--self:SetStatusEnd( self:GetNextPrimaryFire() )
+			self:SetSilenced(not self:GetSilenced())
+			self2.Silenced = self:GetSilenced()
+		elseif stat == TFA.Enum.STATUS_RELOADING_WAIT and self2.Shotgun then
+			if self2.Ammo1(self) <= 0 or self:Clip1() >= self:GetPrimaryClipSize() or self:GetShotgunCancel() then
+				finalstat = TFA.Enum.STATUS_RELOADING_SHOTGUN_END
+				local _, tanim = self2.ChooseShotgunPumpAnim(self)
+				self:SetStatusEnd(ct + self:GetActivityLength(tanim))
+				--self:SetShotgunCancel( false )
+			else
+				finalstat = self2.LoadShell(self)
+			end
+		elseif stat == TFA.Enum.STATUS_RELOADING_SHOTGUN_END and self2.Shotgun then
+			self:SetShotgunCancel(false)
+		elseif self2.GetStat(self, "PumpAction") and stat == TFA.Enum.STATUS_PUMP then
+			self:SetShotgunCancel(false)
+		elseif stat == TFA.Enum.STATUS_SHOOTING and self2.GetStat(self, "PumpAction") then
+			if self:Clip1() == 0 and self2.GetStat(self, "PumpAction").value_empty then
+				--finalstat = TFA.GetStatus("pump_ready")
+				self:SetShotgunCancel(true)
+			elseif (self2.GetStat(self, "Primary.ClipSize") < 0 or self:Clip1() > 0) and self2.GetStat(self, "PumpAction").value then
+				--finalstat = TFA.GetStatus("pump_ready")
+				self:SetShotgunCancel(true)
+			end
+		end
+
+		--self:SetStatusEnd( math.huge )
+		self:SetStatus(finalstat)
+		local smi = self2.Sights_Mode == TFA.Enum.LOCOMOTION_HYBRID or self2.Sights_Mode == TFA.Enum.LOCOMOTION_ANI
+		local spi = self2.Sprint_Mode == TFA.Enum.LOCOMOTION_HYBRID or self2.Sprint_Mode == TFA.Enum.LOCOMOTION_ANI
+		local wmi = self2.Walk_Mode == TFA.Enum.LOCOMOTION_HYBRID or self2.Walk_Mode == TFA.Enum.LOCOMOTION_ANI
+		local cmi = self2.Customize_Mode == TFA.Enum.LOCOMOTION_HYBRID or self2.Customize_Mode == TFA.Enum.LOCOMOTION_ANI
+
+		if (not TFA.Enum.ReadyStatus[stat]) and stat ~= TFA.Enum.STATUS_SHOOTING and stat ~= TFA.Enum.STATUS_PUMP and finalstat == TFA.Enum.STATUS_IDLE and ((smi or spi) or (cst and cmi)) then
+			is = self2.GetIronSights(self, true)
+
+			if (is and smi) or (spr and spi) or (wlk and wmi) or (cst and cmi) then
+				local success, _ = self2.Locomote(self, is and smi, is, spr and spi, spr, wlk and wmi, wlk, cst and cmi, cst)
+
+				if success == false then
+					self:SetNextIdleAnim(-1)
+				else
+					self:SetNextIdleAnim(math.max(self:GetNextIdleAnim(), ct + 0.1))
+				end
+			end
+		end
+
+		self2.LastBoltShoot = nil
+
+		if self:GetBurstCount() > 0 then
+			if finalstat ~= TFA.Enum.STATUS_SHOOTING and finalstat ~= TFA.Enum.STATUS_IDLE then
+				self:SetBurstCount(0)
+			elseif self:GetBurstCount() < self:GetMaxBurst() and self:Clip1() > 0 then
+				self:PrimaryAttack()
+			else
+				self:SetBurstCount(0)
+				self:SetNextPrimaryFire(ct + self2:GetBurstDelay(self))
+			end
+		end
+	end
+
+	if stat == TFA.Enum.STATUS_IDLE and self:GetShotgunCancel() then
+		if self2.GetStat(self, "PumpAction") then
+			if ct > self:GetNextPrimaryFire() and not self:GetOwner():KeyDown(IN_ATTACK) then
+				self2.DoPump(self)
+			end
+		else
+			self:SetShotgunCancel( false )
+		end
+	end
+end
