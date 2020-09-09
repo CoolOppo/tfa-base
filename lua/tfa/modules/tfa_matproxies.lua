b7d9d9c9093ed935cf8eefe4f6d47f912e79f5dc
YuRaNnNzZZ <medved609@yandex.ru>
2020-09-09 18:05:18 +0300

    move envmaptint proxy into shared proxies file

--- lua/tfa/modules/tfa_matproxies.lua
+++ lua/tfa/modules/tfa_matproxies.lua
@@ -158,3 +158,41 @@ matproxy.Add({
 		mat:SetTexture("$basetexture", self.RTMaterial:GetTexture("$basetexture"))
 	end
 })
+
+local Lerp = Lerp
+local RealFrameTime = RealFrameTime
+
+local vector_one = Vector(1, 1, 1)
+
+matproxy.Add({
+	name = "TFA_CubemapTint",
+	init = function(self, mat, values)
+		self.ResultVar = values.resultvar or "$envmaptint"
+		self.MultVar = values.multiplier
+	end,
+	bind = function(self, mat, ent)
+		local tint = vector_one
+
+		if IsValid(ent) then
+			local mult = self.MultVar and mat:GetVector(self.MultVar) or vector_one
+
+			tint = Lerp(RealFrameTime() * 10, mat:GetVector(self.ResultVar), mult * render.GetLightColor(ent:GetPos()))
+		end
+
+		mat:SetVector(self.ResultVar, tint)
+	end
+})
+
+-- VMT Example:
+--[[
+	$envmapmultiplier	"[1 1 1]" // Lighting will be multiplied by this value
+
+	Proxies
+	{
+		TFA_CubemapTint
+		{
+			resultvar	$envmaptint // Write final output to $envmaptint
+			multiplier	$envmapmultiplier // Use our value for default envmap tint
+		}
+	}
+]]
