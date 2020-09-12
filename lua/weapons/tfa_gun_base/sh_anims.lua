600921d5de7a7ae37f41c574a7f17782e012f3be
YuRaNnNzZZ <medved609@yandex.ru>
2020-09-12 03:19:40 +0300

    shared random for blowback angle BECAUSE WHY NOT

--- lua/weapons/tfa_gun_base/sh_anims.lua
+++ lua/weapons/tfa_gun_base/sh_anims.lua
@@ -1049,7 +1049,7 @@ function SWEP:BlowbackFull()
 
 		minang, maxang = self2.GetStat(self, "BlowbackRandomAngleMin"), self2.GetStat(self, "BlowbackRandomAngleMax")
 
-		self2.BlowbackRandomAngle = Angle(math.Rand(minang.p, maxang.p), math.Rand(minang.y, maxang.y), math.Rand(minang.r, maxang.r))
+		self2.BlowbackRandomAngle = Angle(self2.SharedRandom(self, minang.p, maxang.p, "BlowbackRandomAngle_P"), self2.SharedRandom(self, minang.y, maxang.y, "BlowbackRandomAngle_Y"), self2.SharedRandom(self, minang.r, maxang.r, "BlowbackRandomAngle_R"))
 	end
 end
 
