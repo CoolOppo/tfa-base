09f350fa1b1af55b01fc91ad67f47699d668195c
DBotThePony <dbotthepony@yandex.ru>
2020-10-15 17:12:44 +0700

    Fix bokeh dof

--- lua/tfa/modules/cl_tfa_inspection.lua
+++ lua/tfa/modules/cl_tfa_inspection.lua
@@ -61,7 +61,7 @@ if CLIENT then
 		local wep = ply:GetActiveWeapon()
 		if not IsValid(wep) or not wep.IsTFAWeapon then return end
 
-		tfablurintensity = wep:GetNW2Float("InspectingProgress", 0)
+		tfablurintensity = wep:GetInspectingProgress()
 
 		if tfablurintensity > 0.01 then
 			if doblur and doblur:GetBool() then
