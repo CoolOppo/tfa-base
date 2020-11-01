be8178be45dd0aa87c70cab97830db8a193312e3
YuRaNnNzZZ <medved609@yandex.ru>
2020-11-01 18:49:03 +0300

    aimvector is already a vector, not an angle

--- lua/weapons/tfa_gun_base/sh_bullet.lua
+++ lua/weapons/tfa_gun_base/sh_bullet.lua
@@ -39,7 +39,7 @@ local function BallisticFirebullet(ply, bul, ovr, angPreserve)
 	local wep = ply:GetActiveWeapon()
 
 	if TFA.Ballistics and TFA.Ballistics:ShouldUse(wep) then
-		if ballistics_distcv:GetInt() == -1 or util.QuickTrace(ply:GetShootPos(), ply:GetAimVector():Forward() * 0x7fff, ply).HitPos:Distance(ply:GetShootPos()) > (ballistics_distcv:GetFloat() * TFA.Ballistics.UnitScale) then
+		if ballistics_distcv:GetInt() == -1 or util.QuickTrace(ply:GetShootPos(), ply:GetAimVector() * 0x7fff, ply).HitPos:Distance(ply:GetShootPos()) > (ballistics_distcv:GetFloat() * TFA.Ballistics.UnitScale) then
 			bul.SmokeParticle = bul.SmokeParticle or wep.BulletTracer or wep.TracerBallistic or wep.BallisticTracer or wep.BallisticsTracer
 
 			if ovr then
